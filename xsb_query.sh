874e3275484cc749bc3521b1bd412312b09c5bc0
Qiwen Wang <qwang70@illinois.edu>
2016-10-14 10:54:12 -0500

    profiling

--- xsb_query.sh
+++ xsb_query.sh
@@ -5,16 +5,17 @@
 LEVEL_DIR=level_info
 LEVEL=$LEVEL_DIR/level$1
 
-xsb --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
+xsb -p --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
 
 ['$LEVEL'].
 ['downstream'].
 ['general_rules'].
+['xsb_profiling'].
 
 set_prolog_flag(unknown, fail).
 
 rule_banner('print all descendents of the root\n printall(tree_downstream(_, 1)).').
-printall(tree_downstream(_, 1)).
+profile_call(tree_downstream(X, 1)).
 
 rule_banner('count descendents of the root\n printall(count(tree_downstream(_, 1),_)).').
 printall(count(tree_downstream(_, 1),_)).
